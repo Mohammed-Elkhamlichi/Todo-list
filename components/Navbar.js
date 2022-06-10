d0137c5f3e10debc8cf5f2e6381284b15b0d39ed
Mohammed-Elkhamlichi <elkhamlichimedn1et@gmail.com>
2022-06-11 00:29:33 +0100

    initial commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,48 +3,48 @@ import { useEffect, useState } from "react";
 import { useUserContext } from "../context/state";
 
 const Navbar = () => {
-    // controle the nav bar position
-    const [navFixed, setNavFixed] = useState(false);
-    const [windowScrollY, setWindowScrollY] = useState();
-    const [userState, userDispatch] = useUserContext();
-    useEffect(() => {
-        window.onscroll = () => {
-            if (window.scrollY >= 200) {
-                setWindowScrollY(window.scroll);
-                setNavFixed(true);
-            } else {
-                setNavFixed(false);
-            }
-        };
-    }, [windowScrollY]);
+   // controle the nav bar position
+   const [navFixed, setNavFixed] = useState(false);
+   const [windowScrollY, setWindowScrollY] = useState();
+   const [userState, userDispatch] = useUserContext();
+   useEffect(() => {
+      window.onscroll = () => {
+         if (window.scrollY >= 200) {
+            setWindowScrollY(window.scroll);
+            setNavFixed(true);
+         } else {
+            setNavFixed(false);
+         }
+      };
+   }, [windowScrollY]);
 
-    return (
-        <>
-            <nav
-                className={`bg-slate-700 z-50 text-sky-500 flex flex-row justify-start items-center  shadow-md shadow-slate-800 ${
-                    navFixed ? "fixed top-0 right-0  left-0" : ""
-                }`}>
-                <h1 className={`flex-1 m-2   font-bold text-xl text-yellow-500`}>
-                    <Link href="/">
-                        <a>Todo App</a>
-                    </Link>
-                </h1>
-                <ul className="flex flex-row m-2">
-                    <li className="m-2">
-                        <Link href="/users/login">
-                            <a className="p-2  hover:font-bold text-lg hover:animate-bounce">Log In</a>
-                        </Link>
-                    </li>
+   return (
+      <>
+         <nav
+            className={`bg-slate-700 z-50 text-sky-500 flex flex-row justify-start items-center  shadow-md shadow-slate-800 ${
+               navFixed ? "fixed top-0 right-0  left-0" : ""
+            }`}>
+            <h1 className={`flex-1 m-2   font-bold text-xl text-yellow-500`}>
+               <Link href="/">
+                  <a>Todo App</a>
+               </Link>
+            </h1>
+            <ul className="flex flex-row m-2">
+               <li className="m-2">
+                  <Link href="/users/login">
+                     <a className="p-2  hover:font-bold text-lg hover:animate-bounce">Log In</a>
+                  </Link>
+               </li>
 
-                    <li className="m-2">
+               {/* <li className="m-2">
                         <Link href="/users/login">
                             <a className="p-2  hover:font-bold text-lg hover:animate-bounce">Log Out</a>
                         </Link>
-                    </li>
-                </ul>
-            </nav>
-        </>
-    );
+                    </li> */}
+            </ul>
+         </nav>
+      </>
+   );
 };
 
 export default Navbar;
--- components/todos/TodoForm.js
+++ components/todos/TodoForm.js
@@ -3,71 +3,71 @@ import { useState } from "react";
 import { useTodoContext } from "../../context/state";
 
 const TodoForm = ({ todoInput, handleAddTodoForm }) => {
-    // let apiUrl = "http://localhost:3000/api/v1/todos";
-    let apiUrl = "https://todo-list-mem.vercel.app/api/v1/todos";
+//    let apiUrl = "http://localhost:3000/api/v1/todos";
+   let apiUrl = "https://todo-list-mem.vercel.app/api/v1/todos";
 
-    const [todoState, todoDispatch] = useTodoContext();
-    const [todoTitle, setTodoTitle] = useState("");
+   const [todoState, todoDispatch] = useTodoContext();
+   const [todoTitle, setTodoTitle] = useState("");
 
-    const handleUpdateTodoForm = async (e) => {
-        try {
-            if (todoState?.isUpdateTodo) {
-                e.preventDefault();
+   const handleUpdateTodoForm = async (e) => {
+      try {
+         if (todoState?.isUpdateTodo) {
+            e.preventDefault();
 
-                todoDispatch({ isLoading: true });
+            todoDispatch({ isLoading: true });
 
-                axios
-                    .patch(`${apiUrl}/${todoState?.todo._id}`, {
-                        completed: todoState.todo.completed,
-                        title: todoTitle || todoState.todo.title,
-                    })
-                    .then((res) => {
-                        const todo = res.data.todo;
-                        const todos = res.data.todos;
-                        todoDispatch({
-                            type: "FORM_UPDATE_TODO_SUBMIT",
-                            todo,
-                            todos,
-                            todoAlert: { msg: "todo has been update", classes: "bg-green-500" },
-                            isLoading: false,
-                        });
-                        setTodoTitle("");
-                    })
-                    .catch((err) => console.log(err));
-                todoInput.current.value = "";
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    };
-    return (
-        <form
-            onSubmit={(e) => {
-                todoState.isUpdateTodo ? handleUpdateTodoForm(e) : handleAddTodoForm(e);
-            }}
-            id="todo_form"
-            className="flex flex-col sm:flex-row  w-full sm:w-2/3 m-auto my-5 justify-between items-center bg">
-            <div className="w-3/4 mx-1 flex flex-row items-center">
-                <input
-                    onChange={(e) => {
-                        setTodoTitle(e.target.value);
-                    }}
-                    ref={todoInput}
-                    type="text"
-                    name="title"
-                    id="title"
-                    className="px-2 py-2 text-yellow-600 placeholder:text-yellow-600 placeholder:text-lg text-lg w-full outline-0 bod  rounded hover:outline-dotted hover:outline-slate-800 hover:shadow-xl  hover:shadow-slate-850"
-                    placeholder="Your Todo Title"
-                />
-            </div>
-            <div className="w-1/4 mx-1 mt-5 sm:mt-0">
-                <button type="submit" className="bg-yellow-500 my-4 py-3 px-10 rounded">
-                    {todoState?.isUpdateTodo && "Update"}
-                    {todoState?.isAddTodo && "Add"}
-                </button>
-            </div>
-        </form>
-    );
+            axios
+               .patch(`${apiUrl}/${todoState?.todo._id}`, {
+                  completed: todoState.todo.completed,
+                  title: todoTitle || todoState.todo.title,
+               })
+               .then((res) => {
+                  const todo = res.data.todo;
+                  const todos = res.data.todos;
+                  todoDispatch({
+                     type: "FORM_UPDATE_TODO_SUBMIT",
+                     todo,
+                     todos,
+                     todoAlert: { msg: "todo has been update", classes: "bg-green-500" },
+                     isLoading: false,
+                  });
+                  setTodoTitle("");
+               })
+               .catch((err) => console.log(err));
+            todoInput.current.value = "";
+         }
+      } catch (error) {
+         console.log(error);
+      }
+   };
+   return (
+      <form
+         onSubmit={(e) => {
+            todoState.isUpdateTodo ? handleUpdateTodoForm(e) : handleAddTodoForm(e);
+         }}
+         id="todo_form"
+         className="flex flex-col sm:flex-row  w-full sm:w-2/3 m-auto my-5 justify-between items-center bg">
+         <div className="w-11/12 mx-1 flex flex-row items-center">
+            <input
+               onChange={(e) => {
+                  setTodoTitle(e.target.value);
+               }}
+               ref={todoInput}
+               type="text"
+               name="title"
+               id="title"
+               className="px-2 py-2 text-yellow-600 placeholder:text-yellow-600 placeholder:text-lg text-lg w-full outline-0 bod  rounded hover:outline-dotted hover:outline-slate-800 hover:shadow-xl  hover:shadow-slate-850"
+               placeholder="Your Todo Title"
+            />
+         </div>
+         <div className="w-1/4 mx-1 mt-5 sm:mt-0">
+            <button type="submit" className="bg-yellow-500 my-4 py-3 px-10 rounded">
+               {todoState?.isUpdateTodo && "Update"}
+               {todoState?.isAddTodo && "Add"}
+            </button>
+         </div>
+      </form>
+   );
 };
 
 export default TodoForm;
