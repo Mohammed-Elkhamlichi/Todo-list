877d423f9078bd73c15d6443eb8a993d2d667015
Mohammed-Elkhamlichi <elkhamlichimedn1et@gmail.com>
2022-06-17 23:19:32 +0100

    initial commité

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,17 @@
 const Footer = () => {
-   const thisYear = new Date().getFullYear();
-   return (
-      <footer className="bg-slate-700 z-50 text-yellow-600 py-3 text-center items-center fixed bottom-0 right-0 left-0">
-         <h1 className="text-sm">
-            <span className="font-bold">TodoList</span>
-            <span className="font-bold">
-               &copy;
-               {thisYear}
-            </span>
-            <span className="font-mono">By Mohammed-EL-Khamlichi</span>
-         </h1>
-      </footer>
-   );
+    const thisYear = new Date().getFullYear();
+    return (
+        <footer className="bg-slate-700 z-50 text-yellow-600 py-3 text-center items-center absolute bottom-0 right-0 left-0">
+            <h1 className="text-sm">
+                <span className="font-bold">TodoList</span>
+                <span className="font-bold">
+                    &copy;
+                    {thisYear}
+                </span>
+                <span className="font-mono">By Mohammed-EL-Khamlichi</span>
+            </h1>
+        </footer>
+    );
 };
 
 export default Footer;
